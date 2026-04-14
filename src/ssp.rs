f312162c11084a5d34f38549e03acf5014b1140f
Colin Ozanne <colin.ozanne@etudiant.univ-rennes.fr>
2026-04-14 17:28:18 +0200

    feat: add sort optimisation

--- src/ssp.rs
+++ src/ssp.rs
@@ -1,11 +1,24 @@
 /// Subset Sum Problem solver.
 /// Takes the vector size, the target sum, and the vector of values.
 pub fn ssp(size: usize, target: usize, values: Vec<usize>) -> Vec<usize> {
+    // sort the values in descending order
+    let mut values = values;
+    values.sort_by(|a, b| b.cmp(a));
+
     let mut solution = vec![false; size];
     let mut current_sum: usize = 0;
     let mut i: usize = 0;
 
     loop {
+        loop {
+            // Skip values that are too large
+            if i < size && values[i] > target - current_sum {
+                i += 1;
+            } else {
+                break;
+            }
+        }
+
         if current_sum == target {
             return finalise(values, solution);
         }
