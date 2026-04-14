4fd6bb54ec04425baa834ce5b63bc7a3458eddc1
Colin Ozanne <colin.ozanne@etudiant.univ-rennes.fr>
2026-04-14 16:15:10 +0200

    wip: build skeleton

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 mod parser;
+mod ssp;
 
 fn main() {
     let args: Vec<String> = std::env::args().collect();
@@ -8,5 +9,21 @@ fn main() {
     }
 
     let (size, target, values) = parser::parse_input(&args[1]);
-    println!("Size: {}, Target: {}, Values: {:?}", size, target, values);
+    println!("Size: {}, Target: {}", size, target);
+
+    let start = std::time::Instant::now();
+    let result = ssp::ssp(size, target, values);
+    let elapsed = start.elapsed();
+
+    println!("");
+
+    if result.is_empty() {
+        println!("No solution found");
+    } else {
+        println!("Found solution!");
+        println!("Result: {:?}", result);
+    }
+
+    println!("");
+    println!("Time: {:.3?}", elapsed);
 }
